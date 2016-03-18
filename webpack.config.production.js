a64187943477bccb5c6fc6212ea37499f7d5ff9d
Zak Angelle <zaknotzak@gmail.com>
2016-03-18 12:54:13 -0500

    Added extract text for prod build

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
@@ -8,34 +9,43 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.[hash].js',
-    publicPath: '/',
+    publicPath: '/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production'),
+        NODE_ENV: JSON.stringify('production')
       },
     }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         screw_ie8: true,
-        warnings: false,
+        warnings: false
       },
     }),
     new HtmlWebpackPlugin({
       template: './build/index.html',
       filename: 'index.html'
+    }),
+    new ExtractTextPlugin('./style/index.css', {
+      allChunks: true
     })
   ],
   resolve: {
-    extensions: ['', '.js'],
+    extensions: ['', '.js']
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-    }],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+      }
+    ]
   },
 };
